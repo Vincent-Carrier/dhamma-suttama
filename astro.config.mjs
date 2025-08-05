440dd2b77d44898a8c09a8bab51247f79ff6b884
Vincent Carrier <vincentcarrier15@gmail.com>
2025-08-05 13:56:00 -0400

    Add more home page content

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,49 +8,53 @@ import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://horizon.cosmicthemes.com",
-	integrations: [
-		// example auto import component into blog post mdx files
-		AutoImport({
-			imports: [
-				// https://github.com/delucis/astro-auto-import
-				"@components/Admonition/Admonition.astro",
-			],
-		}),
-		mdx(),
-		icon({
-			// I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
-			// https://www.astroicon.dev/reference/configuration#include
-			include: {
-				tabler: [
-					"bulb",
-					"alert-triangle",
-					"flame",
-					"info-circle",
-					"arrow-narrow-left",
-					"arrow-narrow-right",
-					"menu-2",
-					"x",
-					"chevron-down",
-					"category",
-					"calendar-event",
-				],
-			},
-		}),
-		sitemap(),
-		compress({
-			HTML: true,
-			JavaScript: true,
-			CSS: false,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
-		}),
-	],
-	vite: {
-		plugins: [tailwindcss()],
-		// stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
-		build: {
-			assetsInlineLimit: 0,
-		},
-	},
+  site: "https://suttama.dhamma.org",
+  i18n: {
+    locales: ['en', 'fr-CA'],
+    defaultLocale: 'fr-CA'
+  },
+  integrations: [
+    // example auto import component into blog post mdx files
+    AutoImport({
+      imports: [
+        // https://github.com/delucis/astro-auto-import
+        "@components/Admonition/Admonition.astro",
+      ],
+    }),
+    mdx(),
+    icon({
+      // I include only the icons I use. This is because if you use SSR, ALL icons will be included (no bueno)
+      // https://www.astroicon.dev/reference/configuration#include
+      include: {
+        tabler: [
+          "bulb",
+          "alert-triangle",
+          "flame",
+          "info-circle",
+          "arrow-narrow-left",
+          "arrow-narrow-right",
+          "menu-2",
+          "x",
+          "chevron-down",
+          "category",
+          "calendar-event",
+        ],
+      },
+    }),
+    sitemap(),
+    compress({
+      HTML: true,
+      JavaScript: true,
+      CSS: false,
+      Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+      SVG: false, // astro-icon handles this
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    // stop inlining short scripts to fix issues with ClientRouter: https://github.com/withastro/astro/issues/12804
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
 });
--- src/config/navData.json.ts
+++ src/config/navData.json.ts
@@ -1,51 +1,55 @@
 export interface navLinkItem {
-	text: string;
-	link: string;
-	newTab?: boolean; // adds target="_blank" rel="noopener noreferrer" to link
+  text: string | { en: string, fr: string };
+  link: string;
+  newTab?: boolean; // adds target="_blank" rel="noopener noreferrer" to link
 }
 
 export interface navDropdownItem {
-	text: string;
-	dropdown: navLinkItem[];
+  text: string;
+  dropdown: navLinkItem[];
 }
 
 export type navItem = navLinkItem | navDropdownItem;
 
 // note: 1 level of dropdown is supported
 const navConfig: navItem[] = [
-	{
-		text: "Home",
-		link: "/",
-	},
-	{
-		text: "portfolios",
-		link: "/portfolio/",
-	},
-	{
-		text: "Pricing",
-		link: "/#pricing",
-	},
-	{
-		text: "Pages",
-		dropdown: [
-			{
-				text: "Portfolio",
-				link: "/portfolio/couple-1/",
-			},
-			{
-				text: "Legal",
-				link: "/privacy-policy/",
-			},
-			{
-				text: "Elements",
-				link: "/elements/",
-			},
-			{
-				text: "404",
-				link: "/not-a-link/",
-			},
-		],
-	},
+  {
+    text: "Vipassana",
+    link: "/vipassana/",
+  },
+  {
+    text: "Cours",
+    link: "/course/",
+  },
+  {
+    text: "Le Centre",
+    link: "/centre/",
+  },
+  {
+    text: "Pour les enfants",
+    link: "/enfants/",
+  },
+  // {
+  //   text: "Pages",
+  //   dropdown: [
+  //     {
+  //       text: "Portfolio",
+  //       link: "/portfolio/couple-1/",
+  //     },
+  //     {
+  //       text: "Legal",
+  //       link: "/privacy-policy/",
+  //     },
+  //     {
+  //       text: "Elements",
+  //       link: "/elements/",
+  //     },
+  //     {
+  //       text: "404",
+  //       link: "/not-a-link/",
+  //     },
+  //   ],
+  // },
 ];
 
 export default navConfig;
