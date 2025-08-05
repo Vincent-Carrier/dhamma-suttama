c592822f4abe1481e3ad72ca754f09ba7ae20f15
Vincent Carrier <vincentcarrier15@gmail.com>
2025-08-05 15:56:30 -0400

    [WIP]

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,8 +2,64 @@ import { strapi as strapiClient } from '@strapi/client';
 //
 const strapi = strapiClient({ baseURL: `${import.meta.env.STRAPI_URL}/api`, auth: import.meta.env.STRAPI_TOKEN });
 
-export async function getPageData(path: str): any {
-  return await strapi.single(path).find({ locale: Astro.currentLocale })
+export function resolveRichText(nodes) {
+  return nodes.map((c) => resolveRichTextNode(c)).join('');
+}
+
+function resolveRichTextNode(node) {
+  let html = '';
+  const children = node.children ? node.children.map((c) => resolveRichTextNode(c)).join('') : '';
+
+  switch (node.type) {
+    case 'heading':
+      html = `<h${node.level}>${children}</h${node.level}>`;
+      break;
+    case 'text':
+      if (node.text) {
+        let tmp = node.text.replaceAll('\n', '<br />');
+
+        if (node.bold) tmp = `<b>${tmp}</b>`;
+        if (node.code) tmp = `<code>${tmp}</code>`;
+        if (node.italic) tmp = `<i>${tmp}</i>`;
+        if (node.strikethrough) tmp = `<s>${tmp}</s>`;
+        if (node.underline) tmp = `<u>${tmp}</u>`;
+
+        html = tmp;
+      }
+      break;
+    case 'paragraph':
+      html = `<p class="mb-2">${children}</p>`;
+      break;
+    case 'link':
+      html = `<a href="${node.url}">${children}</a>`;
+      break;
+    case 'list':
+      switch (node.format) {
+        case 'ordered':
+          html = `<ol>${children}</ol>`;
+          break;
+        case 'unordered':
+          html = `<ul>${children}</ul>`;
+          break;
+      }
+      break;
+    case 'list-item':
+      html = `<li>${children}</li>`;
+      break;
+    case 'quote':
+      html = `<blockquote>${children}</blockquote>`;
+      break;
+    case 'code':
+      html = `<pre>${children}</pre>`;
+      break;
+    case 'image':
+      if (node.image) {
+        html = `<img src="${node.image.url}" height="${node.image.height}" width="${node.image.width}" alt="${node.image.alternativeText}" />`;
+      }
+      break;
+  }
+
+  return html;
 }
 
 export default strapi
